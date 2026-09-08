9c0b399c0309e4368e31e42dec59e9336553e7af
instra15 <a3026164589@outlook.com>
2026-09-08 16:16:33 +0800

    Initialize

---
+++ src/main/java/com/schoolMarket/SchoolMarketApplication.java
@@ -0,0 +1,13 @@
+package com.schoolMarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolMarketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchoolMarketApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/schoolMarket/SchoolMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.schoolMarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolMarketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
